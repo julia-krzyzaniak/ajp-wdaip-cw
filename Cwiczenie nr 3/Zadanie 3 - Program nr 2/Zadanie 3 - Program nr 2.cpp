0d02c447db187334a86f07b4ddb3e67f5292cf97
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-21 00:50:20 +0100

    Zadanie 3 - Program nr 2

---
+++ Cwiczenie nr 3/Zadanie 3 - Program nr 2/Zadanie 3 - Program nr 2.cpp
@@ -0,0 +1,25 @@
+﻿#define _USE_MATH_DEFINES 
+#include <iostream> 
+#include <climits> 
+#include <conio.h> 
+#include <math.h> 
+
+using namespace std;
+
+int main()
+
+{
+	float r;
+
+	cout << "Podaj promien: ";
+	cin >> r;
+
+	while (r <= 0) {
+
+		cout << "Podaj ponownie promien: ";
+		cin >> r;
+	}
+
+	cout << "Pole kola: " << M_PI * pow(r, 2) << endl;
+
+}
