672a6f30438832917aef474398a97be20c2051ab
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-21 01:23:52 +0100

    Zadanie 3 - Program nr 4

---
+++ Cwiczenie nr 3/Zadanie 3 - Program nr 4/Zadanie 3 - Program nr 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, suma = 0;
+
+	cout << "Podaj liczbe poczatkowa: ";
+	cin >> a;
+	cout << "Podaj liczbe koncowa: ";
+	cin >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		if (i % 2 == 1) 
+			suma = suma + i;
+	}
+
+	cout << "Suma liczb nieparzystych to: " << suma;
+}
+
