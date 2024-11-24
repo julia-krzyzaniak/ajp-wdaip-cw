1cce796f2351fdb65518427605323b57eaf4e6e8
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-24 08:55:34 +0100

    Zadanie 3 - Program nr 5

---
+++ Cwiczenie nr 3/Zadanie 3 - Program nr 5/Zadanie 3 - Program nr 5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, wynik;
+	char odpowiedz;
+
+	cout << "Program na potegowanie dodatnich liczb: " << endl;
+
+	do {
+		cout << endl << "Podaj liczbe: ";
+		cin >> a;
+
+		cout << "Podaj potege do ktorej ma byc podniesiona liczba: ";
+		cin >> b;
+
+		wynik = pow(a, b);
+		cout << "Wynik: " << wynik << endl;
+
+		cout << endl << "Czy chcesz kontynuowac? Wcisnij t lub T: ";
+		cin >> odpowiedz;
+
+	}
+	while (odpowiedz == 't' || odpowiedz == 'T');
+
+}
+
