61e17f2c4ba1dea9a7f2f6c8611d823d16790490
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-24 10:32:45 +0100

    Zadanie 3 - Program nr 9

---
+++ Cwiczenie nr 3/Zadanie 3 - Program nr 9/Zadanie 3 - Program nr 9.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+
+{
+    srand(time(NULL));
+
+	int i, j, liczba;
+
+	for (int i = 1; i <= 8; i++) {
+		for (int j = 1; j <= 5; j++) {
+			liczba = (rand() % 100);
+			cout << liczba << " ";
+		}
+		cout << endl;
+
+	}
+
+
+}
+
