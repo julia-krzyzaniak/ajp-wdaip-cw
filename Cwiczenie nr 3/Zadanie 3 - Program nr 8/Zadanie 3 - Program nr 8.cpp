629349ab26e32745a1ccd126f3511589a46f6e15
Julia Krzyzaniak <julia.krzyzaniak98@gmail.com>
2024-11-24 10:21:11 +0100

    Zadanie 3 - Program nr 8

---
+++ Cwiczenie nr 3/Zadanie 3 - Program nr 8/Zadanie 3 - Program nr 8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int  a = 0, b = 0;
+
+	//cout << "Podaj pierwsza liczbe: ";
+	//cin >> a;
+
+	//cout << "Podaj druga liczbe: ";
+	//cin >> b;
+
+	for (int i = 0; i <= 10; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+
+	}
+}
